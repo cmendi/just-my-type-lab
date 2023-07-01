a5f2becdb9fd62ae008e1778db6e34fc871dfd4c
mendi <mendenhall_chris1246@yahoo.com>
2023-06-30 19:43:49 -0700

    Finished step 4

--- app.js
+++ app.js
@@ -1,3 +1,32 @@
 $(document).ready(function () {
-	console.log("working");
+	// Variables
+	let keyUpper = $("#keyboard-upper-container");
+	let keyLower = $("#keyboard-lower-container");
+
+	// Hide uppcase keyboard when page loads
+	$(keyUpper).hide();
+
+	// Create event listener that listens for a keydown.
+	$(document).keydown(function (e) {
+		// console.log(e1);
+
+		if (e.which === 16) {
+			$(keyLower).hide();
+			$(keyUpper).show();
+		}
+	});
+
+	$(document).keyup(function (e) {
+		if (e.which === 16) {
+			$(keyLower).show();
+			$(keyUpper).hide();
+		}
+		$(".background-yellow").removeClass("background-yellow");
+	});
+
+	$(document).keypress(function (e) {
+		$("#" + e.which).addClass("background-yellow");
+
+		// console.log(e.which);
+	});
 });
