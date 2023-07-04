d9686c9d9481266dc60c7c5fb623931eff9d63c7
mendi <mendenhall_chris1246@yahoo.com>
2023-07-03 22:38:15 -0700

    yellowblock movement, current letter next sentence

--- app.js
+++ app.js
@@ -11,11 +11,15 @@ $(document).ready(function () {
 	];
 	let letterIndex = 0;
 	let sentenceIndex = 0;
-	// console.log(sentences);
+	let numberOfWords = 54;
 	// console.log(sentences[0]);
-	// Create a variable with the first sentence of the sentences array and display it on the page
-	let currentSentence = sentences[0];
-	$("#sentence").append(currentSentence);
+	// Create a variable with the first sentence of the sentences array and display it on the page and current letter in the sentence
+	let currentSentence = sentences[sentenceIndex];
+	// console.log(currentSentence.length);
+	let currentLetter = currentSentence[letterIndex];
+	let targetLetterDiv = $("#target-letter");
+	$("#sentence").text(sentences[sentenceIndex]);
+	targetLetterDiv.text(currentLetter);
 
 	// Hide the uppercase keyboard when page loads
 	$(keyUp).toggle();
@@ -25,8 +29,8 @@ $(document).ready(function () {
 		// console.log(e);
 		// When pressing shift toggle keyboards to show and hide
 		if (e.which === 16) {
-			$(keyUp).toggle();
-			$(keyLow).toggle();
+			$(keyLow).hide();
+			$(keyUp).show();
 		}
 	});
 
@@ -34,8 +38,8 @@ $(document).ready(function () {
 	$(document).keyup(function (e) {
 		// when pressing shift toggle keyboards to show and hide
 		if (e.which === 16) {
-			$(keyLow).toggle();
-			$(keyUp).toggle();
+			$(keyLow).show();
+			$(keyUp).hide();
 		}
 		$(".background-yellow").removeClass("background-yellow");
 	});
@@ -52,12 +56,39 @@ $(document).ready(function () {
 		let currentLetter = currentSentence[letterIndex];
 		// Create a counter to go to the next letter index when key is pressed
 		letterIndex++;
-		console.log(letterIndex);
-		// Check and see if the letter that has been pressed matches the letter index in the sentence
-		if (e.which === currentLetter.charCodeAt()) {
-			console.log("Correct");
-		} else {
-			console.log("Wrong");
+		let targetLetter = currentSentence[letterIndex];
+		targetLetterDiv.text(targetLetter);
+		// Move yellow block across the screen when a key is pressed.
+		$("#yellow-block").animate({ left: "+=17.5px" }, { duration: 1 });
+
+		// if (sentenceIndex < sentences.length) {
+		// console.log("running one");
+		if (letterIndex < currentSentence.length) {
+			console.log("running two");
+			// Check and see if the letter that has been pressed matches the letter index in the sentence
+			if (e.which === currentLetter.charCodeAt()) {
+				// console.log("Correct");
+				// Target the foodback div and enter a checkmark
+				$("#feedback").append('<span class="glyphicon glyphicon-ok"></span>');
+			} else {
+				// console.log("Wrong");
+				$("#feedback").append(
+					'<span class="glyphicon glyphicon-remove"></span>'
+				);
+			}
+			// Once sentence is complete go to the next sentence and reset yellow block to the start
+		} else if (sentenceIndex < sentences.length - 1) {
+			console.log(sentenceIndex);
+			$("#feedback").empty();
+			sentenceIndex++;
+			$("#sentence").text(sentences[sentenceIndex]);
+			targetLetterDiv.text(sentences[sentenceIndex].charAt(0));
+			letterIndex = 0;
+			$("#yellow-block").animate({ left: "15px" });
+			// Game over
+		} else if (sentenceIndex < sentences.length) {
+			console.log("Game Over");
 		}
+		// }
 	});
 });
